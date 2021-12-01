60b7bc931d0fbcba07fe46c0667b036102af388f
Tim Adler <tim@emrys-merlin.de>
2021-12-01 19:35:40 +0100

    More beautiful solution

--- 2021/01/main.py
+++ 2021/01/main.py
@@ -4,26 +4,11 @@ from typing import List, Union
 from pathlib import Path
 
 
-def count_increases(a: List[int]) -> int:
-    """Count consecutive increases."""
-    if len(a) < 1:
-        return 0
-
-    result = 0
-    for current, prev in zip(a[1:], a[:-1]):
-        result += (current > prev)
-
-    return result
-
-
 def count_sum_increases(a: List[int], n: int = 3) -> int:
     """Count consecutive increases of sums."""
-    new_sum = sum(a[:n])
     result = 0
     for pop, push in zip(a[:-n], a[n:]):
-        old_sum = new_sum
-        new_sum = old_sum + push - pop
-        result += (new_sum > old_sum)
+        result += (push > pop)
 
     return result
 
@@ -41,7 +26,7 @@ def main(path: Union[str, Path]):
             if len(line.strip()) != 0
         ]
 
-    result = count_increases(a)
+    result = count_sum_increases(a, 1)
     print(f'Task 1 result: {result}')
 
     result = count_sum_increases(a)
