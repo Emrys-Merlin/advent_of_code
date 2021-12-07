dde7ed7ba98d2a2249311bc56360e854ad95a024
Tim Adler <t.adler@dkfz.de>
2021-12-07 09:04:35 +0100

    Solution for day 07

---
+++ 2021/07/main.py
@@ -0,0 +1,80 @@
+"""Solutions on day 07.
+
+https://adventofcode.com/2021/day/7
+"""
+from pathlib import Path
+from typing import Callable, Tuple, Union
+
+import click
+import numpy as np
+from tqdm import tqdm
+
+
+def fuel_task01(positions: np.ndarray, final_position: int) -> int:
+    """Metric for task 01."""
+    return np.abs(positions - final_position).sum()
+
+
+def fuel_task02(positions: np.ndarray, final_position: int) -> int:
+    """Metric for task 02."""
+    dist = np.abs(positions - final_position)
+    return (dist*(dist+1)//2).sum()
+
+
+def optimize_fuel(positions: np.ndarray, metric: Callable) -> Tuple[int, int]:
+    """Optimize fuel for both tasks depending on metric."""
+    left = positions.min()
+    right = positions.max()
+
+    fuel = np.inf
+    minimal_pos = -1
+    for pos in tqdm(range(left, right+1)):
+        new_fuel = metric(positions, pos)
+        if new_fuel < fuel:
+            fuel = new_fuel
+            minimal_pos = pos
+
+    return fuel, minimal_pos
+
+
+def task01(positions: np.ndarray) -> Tuple[int, int]:
+    """Solves task 01."""
+    return optimize_fuel(positions, fuel_task01)
+
+
+def task02(positions: np.ndarray) -> Tuple[int, int]:
+    """Solves task 02."""
+    return optimize_fuel(positions, fuel_task02)
+
+
+@click.command()
+@click.argument('path', type=click.Path())
+def main(path: Union[str, Path]):
+    """Solve day 07 tasks.
+
+    Read input from PATH and and apply solutions
+    to day 07 tasks. Prints the solution.
+    \f
+
+    :param path: Path to the input file
+    """
+    path = Path(path)
+    with open(path, 'r') as f:
+        line = f.readline()
+
+    positions = np.array(line.strip().split(','), dtype='int')
+    print(f'{len(positions)=}')
+    print(f'{positions.min()=}')
+    print(f'{positions.max()=}\n')
+
+    fuel, position = task01(positions)
+    print(f'{fuel=}')
+    print(f'{position=}')
+
+    fuel, position = task02(positions)
+    print(f'{fuel=}')
+    print(f'{position=}')
+
+
+if __name__ == '__main__':
+    main()
