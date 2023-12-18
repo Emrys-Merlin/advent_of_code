9a248aea85fd4a020d30d39cb90a876053fe460d
Tim Adler <tim@hema.to>
2023-12-18 18:09:13 +0100

    Complete python solution for day 18

--- 2023/python/day18.py
+++ 2023/python/day18.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from itertools import product
 from pathlib import Path
-from typing import Iterator, Literal
+from typing import Iterable, Literal
 
 from loguru import logger
 from typer import Typer
@@ -21,6 +20,8 @@ ALTERNATIVE_DIRECTIONS = list("RDLU")
 
 @dataclass
 class Instruction:
+    """Instruction parser"""
+
     direction: Literal["R", "L", "D", "U"]
     step_size: int
     color: str
@@ -30,137 +31,123 @@ class Instruction:
         direction, step_str, color = line.strip().split()
         return cls(direction, int(step_str), color[2:-1])
 
-    def dig(
-        self, start: tuple[int, int], alternative: bool = False
-    ) -> Iterator[tuple[int, int]]:
-        """Dig a straight line"""
+    def get_direction_step_size(self, alternative: bool = False) -> tuple[str, int]:
+        """Depending on the alternative, return the direction and step size
+
+        Args:
+            alternative: If False, simply return direction and step_size. Otherwise,
+            parse the hex number to get the values. Defaults to False.
+
+        Returns:
+            (direction("R", "L", "D", "U"), step_size(int))
+        """
         if alternative:
             direction = ALTERNATIVE_DIRECTIONS[int(self.color[-1])]
             step_size = int(self.color[:-1], base=16)
         else:
             direction = self.direction
             step_size = self.step_size
+
+        return direction, step_size
+
+    def new_corner(
+        self, start: tuple[int, int], alternative: bool = False
+    ) -> tuple[int, int]:
+        """Return the new corner of the polygon following the instructions
+
+        Args:
+            start: Start point
+            alternative: How to parse instructions. Defaults to False.
+
+        Returns:
+            New corner
+        """
+        direction, step_size = self.get_direction_step_size(alternative=alternative)
         delta_row, delta_col = DIRECTION[direction]
 
-        for step in range(1, step_size + 1):
-            row = start[0] + step * delta_row
-            col = start[1] + step * delta_col
-            yield (row, col)
+        return (start[0] + step_size * delta_row, start[1] + step_size * delta_col)
 
 
-class Grid:
-    def __init__(self, input: str, alternative: bool = False):
-        current = (0, 0)
-        self.trench: set[tuple[int, int]] = {current}
-        self.instructions: list[Instruction] = []
-        top_left = (0, 0)
-        bottom_right = (0, 0)
-        for line in input.splitlines():
-            instruction = Instruction.from_line(line)
-            self.instructions.append(instruction)
+def shoelace_formula(points: Iterable[tuple[int, int]]) -> int:
+    """Closed formula for the area of a polygon based on the coordinates of its vertices
 
-            for coord in instruction.dig(current, alternative=alternative):
-                self.trench.add(coord)
-                top_left = (
-                    min(top_left[0], coord[0]),
-                    min(top_left[1], coord[1]),
-                )
-                bottom_right = (
-                    max(bottom_right[0], coord[0]),
-                    max(bottom_right[1], coord[1]),
-                )
+    Args:
+        points: Vertices (or corners) of the polygon
 
-            current = coord
-
-        self.top_left = top_left
-        self.bottom_right = bottom_right
-        self.fill()
-
-    def _dfs(self, start: tuple[int, int]) -> set[tuple[int, int]]:
-        stack = [start]
-        visited: set[tuple[int, int]] = set()
-
-        while stack:
-            node = stack.pop()
-
-            if (
-                node in visited
-                or node in self.trench
-                or node[0] < self.top_left[0]
-                or node[0] > self.bottom_right[0]
-                or node[1] < self.top_left[1]
-                or node[1] > self.bottom_right[1]
-            ):
-                continue
-
-            visited.add(node)
-
-            for delta_row, delta_col in product([-1, 0, 1], repeat=2):
-                stack.append((node[0] + delta_row, node[1] + delta_col))
-
-        return visited
-
-    def fill(self):
-        start = next(iter(self.trench))
-        for delta_row, delta_col in product([-1, 0, 1], repeat=2):
-            row = start[0] + delta_row
-            col = start[1] + delta_col
-            if (row, col) in self.trench:
-                continue
-
-            possible_fill = self._dfs((row, col))
-            if all(
-                self.top_left[0] < row < self.bottom_right[0]
-                and self.top_left[1] < col < self.bottom_right[1]
-                for row, col in possible_fill
-            ):
-                self.trench.update(possible_fill)
-                return
-
-        raise Exception("Could not fill")
-
-    @property
-    def width(self) -> int:
-        return self.bottom_right[1] - self.top_left[1] + 1
-
-    @property
-    def height(self) -> int:
-        return self.bottom_right[0] - self.top_left[0] + 1
-
-    @property
-    def trench_size(self) -> int:
-        return len(self.trench)
-
-    def __str__(self) -> str:
-        return "\n".join(
-            "".join(
-                "#" if (row, col) in self.trench else "."
-                for col in range(self.top_left[1], self.bottom_right[1] + 1)
-            )
-            for row in range(self.top_left[0], self.bottom_right[0] + 1)
-        )
+    Returns:
+        Area of the polygon
+    """
+    x, y = zip(*points)
+    return (
+        abs(sum(x[i] * y[i + 1] - x[i + 1] * y[i] for i in range(-1, len(x) - 1))) // 2
+    )
 
 
-# 43734 too high
+def area_with_boundary(input: str, alternative: bool = False) -> int:
+    """Area of the polygon
+
+    Based on Pick's theorem. The area of a polygon whose vertices are grid points is
+    A = i + b/2 - 1, where i is the number of interior points and b is the number of
+    boundary points.
+    As the "area" we are looking for is the number of points in the polygon (including the
+    boundary), we can rewrite the formula as i + b = A + 1 + b/2, where we get A from
+    the shoelace formula and b by counting the boundary points.
+
+    Thanks to https://github.com/fspoettel, who showed me this approach.
+
+    Args:
+        input: Input instructions
+        alternative: How the instructions are parsed. False, for task01, True, for task02.
+        Defaults to False.
+
+    Returns:
+        Area of the polygon (in the sense that all points in the polygon (including boundary) are counted)
+    """
+
+    border = 0
+    points: list[tuple[int, int]] = [(0, 0)]
+    for line in input.splitlines():
+        instruction = Instruction.from_line(line)
+        border += instruction.get_direction_step_size(alternative=alternative)[1]
+        points.append(instruction.new_corner(points[-1], alternative=alternative))
+
+    return shoelace_formula(points) + 1 + border // 2
+
+
 @timer
 def task01(input: str) -> int:
-    grid = Grid(input)
-    return grid.trench_size
+    """Solution to task01
+
+    Args:
+        input: Input instructions as string
+
+    Returns:
+        Area
+    """
+    return area_with_boundary(input)
 
 
 @timer
 def task02(input: str) -> int:
-    grid = Grid(input, alternative=True)
-    return grid.trench_size
+    """Solution to task02
+
+    Args:
+        input: Input instructions as string
+
+    Returns:
+        Area
+    """
+    return area_with_boundary(input, alternative=True)
 
 
 @main.command()
 def entrypoint(path: Path):
+    """CLI entrypoint"""
     with open(path, "r") as f:
         input = f.read().strip()
 
     logger.info(f"Task01: {task01(input)}")
-    # logger.info(f"Task02: {task02(input)}")
+    logger.info(f"Task02: {task02(input)}")
 
 
 if __name__ == "__main__":
--- 2023/python/test_day18.py
+++ 2023/python/test_day18.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from day18 import task01, task02
+from day18 import shoelace_formula, task01, task02
 from pytest import fixture
 
 
@@ -10,6 +10,14 @@ def input1() -> str:
 
     with open(path, "r") as f:
         return f.read().strip()
+
+
+def test_polygon_area_formula():
+    points = [(0, 0), (0, 3), (4, 3)]
+    assert shoelace_formula(points) == 6
+
+    points = [(0, 0), (1, 0), (1, 1), (0, 1)]
+    assert shoelace_formula(points) == 1
 
 
 def test_task01(input1: str):
