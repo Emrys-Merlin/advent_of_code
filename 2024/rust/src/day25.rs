aa3638c1d873e6304d10a1e6de284e98cb522869
Tim Adler <tim@emrys-merlin.de>
2024-12-25 08:29:32 +0100

    Solution for 2024-12-25 in rust

---
+++ 2024/rust/src/day25.rs
@@ -0,0 +1,60 @@
+
+fn parse_input(input: &str) -> (Vec<[usize; 5]>, Vec<[usize; 5]>) {
+    let mut locks = Vec::new();
+    let mut keys = Vec::new();
+
+    let mut is_lock = true;
+    let mut key_or_lock = [0; 5];
+    for (i, line) in input.lines().enumerate() {
+        match i % 8 {
+            7 => {
+                if is_lock {
+                    locks.push(key_or_lock);
+                } else {
+                    keys.push(key_or_lock);
+                }
+                key_or_lock = [0; 5];
+                continue
+            },
+            0 => {
+                is_lock = line.starts_with("#");
+            },
+            _ => { },
+        }
+
+        for (j, char) in line.chars().enumerate() {
+            if char == '#' {
+                key_or_lock[j] += 1;
+            }
+        }
+    }
+
+    if is_lock {
+        locks.push(key_or_lock);
+    } else {
+        keys.push(key_or_lock);
+    }
+
+    (locks, keys)
+
+}
+
+fn add_up(lock: &[usize; 5], key: &[usize; 5]) -> bool {
+    (0..5).all(|i| {
+        lock[i] + key[i] <= 7
+    })
+}
+
+pub fn task01(input: &str) -> String {
+    let (locks, keys) = parse_input(input);
+
+    locks.iter().flat_map(|lock| {
+        keys.iter().filter_map(move |key| {
+            if add_up(lock, key) {
+                Some(true)
+            } else {
+                None
+            }
+        })
+    }).count().to_string()
+}
--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -26,6 +26,7 @@ mod day21;
 mod day22;
 mod day23;
 mod day24;
+mod day25;
 
 pub fn get_task_map() -> HashMap<(i32, i32), fn(&str) -> String> {
     let mut task_map: HashMap<(i32, i32), fn(&str) -> String> = HashMap::new();
@@ -77,6 +78,8 @@ pub fn get_task_map() -> HashMap<(i32, i32), fn(&str) -> String> {
     task_map.insert((23, 2), day23::task02);
     task_map.insert((24, 1), day24::task01);
     task_map.insert((24, 2), day24::task02);
+    task_map.insert((25, 1), day25::task01);
+    // task_map.insert((25, 2), day25::task02);
     task_map
 }
 
