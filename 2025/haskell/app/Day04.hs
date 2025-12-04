1bf6cd1f8ab9e38900383772945cd8e73e67c8e7
Tim Adler <tim@emrys-merlin.de>
2025-12-04 08:29:22 +0100

    Use neighbors as list not set

--- 2025/haskell/app/Day04.hs
+++ 2025/haskell/app/Day04.hs
@@ -22,20 +22,21 @@ recursiveAccessiblePaperRole coord_set =
   let new_coord_set = Set.filter (`isNotAccessible` coord_set) coord_set
       accessible = length coord_set - length new_coord_set
   in accessible + if accessible /= 0
-    then recursiveAccessiblePaperRole new_coord_set
-    else 0
+     then recursiveAccessiblePaperRole new_coord_set
+     else 0
 
 isNotAccessible :: Coord -> Set.Set Coord -> Bool
 isNotAccessible coord coords = not $ isAccessible coord coords
 
 isAccessible :: Coord -> Set.Set Coord -> Bool
-isAccessible coord coords = length (Set.intersection neighbors coords) < 4
+isAccessible coord coords = length filled_neighbors < 4
   where neighbors = get8Neighbors coord
+        filled_neighbors = [x | x <- neighbors, Set.member x coords]
 
 extractCoords :: String -> [Coord]
 extractCoords content =
   let rows = zip (lines content) [0..]
   in [(i, j) | (row, i) <- rows, (c, j) <- zip row [0..], c == '@']
 
-get8Neighbors :: Coord -> Set.Set Coord
-get8Neighbors (i, j) = Set.fromList [(i + di, j + dj) | di <- [-1, 0, 1], dj <- [-1, 0, 1], di /= 0 || dj /= 0]
+get8Neighbors :: Coord -> [Coord]
+get8Neighbors (i, j) = [(i + di, j + dj) | di <- [-1, 0, 1], dj <- [-1, 0, 1], di /= 0 || dj /= 0]
